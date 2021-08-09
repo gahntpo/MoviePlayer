a8373dd570157b60d28ab842abd572b035718ed1
Karin Prater <karin.prater@gmx.de>
2021-08-09 16:45:01 +0300

    changed folder name to capital letters

---
+++ MoviePlayer/Main/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  MoviePlayer
+//
+//  Created by Karin Prater on 04.08.21.
+//
+
+import SwiftUI
+
+enum TabSelection {
+    case movies
+    case favorites
+    case settings
+    
+}
+
+struct ContentView: View {
+    
+    @State private var selection: TabSelection = .movies
+    
+    @StateObject var favorties = FavoritesManager()
+    @StateObject var fetcher = MovieFetcher()
+    
+  
+    var body: some View {
+        
+        if fetcher.isLoading {
+            
+            VStack(spacing: 20) {
+                ProgressView()
+                   
+                Text("Loading movies")
+                    .font(.headline)
+            }
+            
+        }else if fetcher.error != nil {
+            
+           ErrorView(fetcher: fetcher)
+            
+        }else {
+            
+            TabView(selection: $selection) {
+                MoviesView(fetcher: fetcher)
+                    .tabItem { Label("Movies", image: selection == .movies ? "MovieLight" : "MovieDisabled") }
+                    .tag(TabSelection.movies)
+                
+                FavoritesView()
+                    .tabItem { Label("Favorites", image: selection == .favorites ? "FavoritesLight" : "FavoritesDisabled") }
+                    .tag(TabSelection.favorites)
+                
+                SettingsView()
+                    .tabItem { Label("Settings", image: selection == .settings ? "SettingsLight" : "SettingsDisabled") }
+                    .tag(TabSelection.settings)
+                
+                
+                
+            }
+            .environmentObject(favorties)
+         
+        }
+      
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        ContentView()
+        
+    }
+}
